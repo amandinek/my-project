a946e909387c9720eaf08a4cd6c2b852d682d716
amandinek <karambiziga@gmail.com>
2019-11-18 18:19:22 +0200

    add js

---
+++ js/js.js
@@ -0,0 +1,29 @@
+////////////////////////////////toggle////////////////////////////////////////////////////////////////////////////////////////////////
+$(document).ready(function(){ 
+  $("#img1").mouseenter (function (){
+    $("#p1").show();  
+  });
+  $("#img1").mouseleave (function (){
+    $("#p1").hide();  
+  });
+  $("#img2").mouseenter (function (){
+    $("#p2").show();  
+  });
+  $("#img2").mouseleave (function (){
+    $("#p2").hide();  
+  });
+  $("#img3").mouseenter (function (){
+    $("#p3").show();  
+  });
+  $("#img3").mouseleave (function (){
+    $("#p3").hide();  
+  });
+  $("#img4").mouseenter (function (){
+    $("#p4").show();  
+  });
+  $("#img4").mouseleave (function (){
+    $("#p4").hide();  
+  });
+  
+});
+    
